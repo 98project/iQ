75c4b6ab6b7cb01c430e63bed559bf6479418ef1
98project <98project@hotmail.com.tw>
2012-07-20 14:22:55 +0800

    Initial Commit

---
+++ iQ/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iQ
+//
+//  Created by Tim Chen on 12/7/20.
+//  Copyright (c) 2012年 NCCU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iQ/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iQ
+//
+//  Created by Tim Chen on 12/7/20.
+//  Copyright (c) 2012年 NCCU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
